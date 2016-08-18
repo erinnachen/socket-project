5a8aa778d6502a7a12243d06ff500fe6f6126b5c
Erinna Chen <erinna.chen@gmail.com>
2016-08-17 19:56:07 -0600

    Add sending votes to server for ask-the-audience

--- ask-the-audience/server.js
+++ ask-the-audience/server.js
@@ -1,8 +1,10 @@
 "use strict";
 const http = require('http');
 const express = require('express');
+const socketIo = require('socket.io');
 
 const app = express();
+var votes = {};
 
 app.use(express.static('public'));
 
@@ -16,5 +18,29 @@ const server = http.createServer(app)
                    .listen(port, function() {
                      console.log('Listening on port ' +  port + '.');
                    });
+const io = socketIo(server);
+
+io.on('connection', function (socket) {
+  console.log('A user has connected.', io.engine.clientsCount);
+
+  io.sockets.emit('usersConnected', io.engine.clientsCount);
+
+  socket.emit('statusMessage', 'You are now connected.');
+
+  socket.on('disconnect', function (socket) {
+    console.log('A user has disconnected.', io.engine.clientsCount);
+    delete votes[socket.id];
+    console.log(votes);
+    io.sockets.emit('usersConnected', io.engine.clientsCount);
+  });
+
+  socket.on('message', function(channel, message) {
+    if (channel === 'voteCast') {
+      votes[socket.id] = message;
+      console.log(votes);
+    }
+  });
+});
+
 
 module.exports = server;
